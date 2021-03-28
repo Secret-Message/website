bc5928466f4868de3c35e7868e499f8467ffa921
Zosiamm <79708932+Zosiamm@users.noreply.github.com>
2021-03-28 13:12:45 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-var DownloadText = "Pobierz aplikację";
-    if (navigator.appVersion.indexOf("Win") != -1) DownloadText = "Pobierz dla systemu windows";
-    if (navigator.appVersion.indexOf("Mac") != -1) DownloadText = "Pobierz dla systemu Mac OS";
-    if (navigator.appVersion.indexOf("Linux") != -1) DownloadText = "Pobierz dla systemu Linux";
-    document.getElementById("download-os").innerText = DownloadText;
-
-    
